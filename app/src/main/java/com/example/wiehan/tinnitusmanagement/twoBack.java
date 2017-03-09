1c34ea70ab9fd7f596a57c8c5d2eaf79d312ae23
Wiehan <boshoff.2@wright.edu>
2017-03-09 17:50:21 -0500

    2-back feature done !

--- app/src/main/java/com/example/wiehan/tinnitusmanagement/twoBack.java
+++ app/src/main/java/com/example/wiehan/tinnitusmanagement/twoBack.java
@@ -22,8 +22,12 @@ public class twoBack extends AppCompatActivity {
     TextView correctText ;
     TextView wrongText ;
 
-    int countSeq = 0 ; ;
     Button tapButton;
+    Button exitButton ;
+    Button startSequence ;
+
+    int countSeq = 0 ; ;
+
     int track = 0 ;
     int correct = 0 ;
     int wrong = 0 ;
@@ -33,21 +37,11 @@ public class twoBack extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_back);
 
-        final Button startSequence = (Button) findViewById(R.id.buttonStartSequence);
-        final Button exitButton = (Button) findViewById(R.id.exitButton);
+        startSequence = (Button) findViewById(R.id.buttonStartSequence);
+        exitButton = (Button) findViewById(R.id.exitButton);
         tapButton = (Button) findViewById(R.id.buttonTap);
 
-        //Create new randomize object
-        Random rand = new Random();
-
-        //Choose a random letter out of the string and add it to the sequence array.
-        String letters = "ABCDEF";
-        sequence = new Character[6];
-
-
-        for (int i = 0; i < sequence.length; i++) {
-            sequence[i] = letters.charAt(rand.nextInt(5));
-        }
+        sequence = new Character[15];
 
         correctText = (TextView) findViewById(R.id.textCorrect) ;
         wrongText = (TextView) findViewById(R.id.textWrong) ;
@@ -60,19 +54,21 @@ public class twoBack extends AppCompatActivity {
         tvCount.setText("");
         seqText.setText("");
         tapButton.setVisibility(View.INVISIBLE);
+        correctText.setVisibility(View.INVISIBLE);
+        wrongText.setVisibility(View.INVISIBLE);
 
         //Changes screens when button is clicked.
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                exitPressed(exitButton);
+                exitPressed();
             }
         });
 
         startSequence.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                bounceButton(startSequence, exitButton);
+                bounceButton();
             }
         });
 
@@ -86,9 +82,9 @@ public class twoBack extends AppCompatActivity {
     }
 
     //If continue button is pressed go back to the selector screen.
-    public void exitPressed(final Button button) {
+    public void exitPressed() {
         final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
-        button.startAnimation(myAnim);
+        exitButton.startAnimation(myAnim);
 
         // Use bounce interpolator with amplitude 0.2 and frequency 20
         // 0.2 is the bounce amplitude and 20 is the frequency
@@ -120,6 +116,16 @@ public class twoBack extends AppCompatActivity {
             count = 3;
             tvCount.setText(""); //Note: the TextView will be visible again here.
             tapButton.setVisibility(View.VISIBLE);
+            correct = 0 ;
+            wrong = 0 ;
+
+            /* Call radomizeSeq function with a string of letters. Random letters are chosen out of
+               the string and added to the sequence array.*/
+            randomizeSeq("ABCDEFG");
+
+            correctText.setVisibility(View.INVISIBLE);
+            wrongText.setVisibility(View.INVISIBLE);
+
             sequenceRepeat();
             return;
         }
@@ -151,7 +157,7 @@ public class twoBack extends AppCompatActivity {
 
     }
 
-    public void bounceButton(final Button startSequence, final Button exitButton) {
+    public void bounceButton() {
         //Animation for button bounce
         final Animation myAnim = AnimationUtils.loadAnimation(this, R.anim.bounce);
         MyBounceInterpolator interpolator = new MyBounceInterpolator(0.3, 25);
@@ -174,6 +180,10 @@ public class twoBack extends AppCompatActivity {
                 startSequence.setVisibility(View.INVISIBLE);
                 exitButton.setVisibility(View.INVISIBLE);
                 explain.setVisibility(View.INVISIBLE);
+                explain.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+                correctText.setVisibility(View.INVISIBLE);
+                wrongText.setVisibility(View.INVISIBLE);
+
                 startPressed();
             }
         });
@@ -185,14 +195,21 @@ public class twoBack extends AppCompatActivity {
         if(countSeq == sequence.length){
             countSeq = 0 ;
             seqText.setText("") ;
-            correctText.setText(correct+"") ;
-            wrongText.setText(wrong+"") ;
+            correctText.setText("CORRECT: " + correct) ;
+            wrongText.setText("WRONG: " + wrong);
+            startSequence.setVisibility(View.VISIBLE);
+            exitButton.setVisibility(View.VISIBLE);
+            tapButton.setVisibility(View.INVISIBLE);
+            explain.setText("YOUR SCORES ARE IN\nTO PLAY AGAIN PRESS START\nOTHERWISE PRESS EXIT");
+            explain.setVisibility(View.VISIBLE);
+            correctText.setVisibility(View.VISIBLE);
+            wrongText.setVisibility(View.VISIBLE);
             return ;
         }
 
         //Animation for sequence
         AlphaAnimation myAnim3 = new AlphaAnimation(1.0f, 0.0f);
-        myAnim3.setDuration(1000);
+        myAnim3.setDuration(750);
 
         seqText.setText(sequence[countSeq].toString());
 
@@ -210,18 +227,20 @@ public class twoBack extends AppCompatActivity {
             //Calls sartPressed method again till count down hits 0
             @Override
             public void onAnimationEnd(Animation animation) {
-            if(countSeq > 0) {
-                if (track == 1) {
-                    int flag = 0 ;
-                    for (int i = 0; i < countSeq; i++) {
-                        if (sequence[countSeq] == sequence[i]) {
-                            flag = 1 ;
-                            correct++;
-                            break;
+            if(!(countSeq % 2 == 1)) {
+                if (countSeq > 0) {
+                    if (track == 1) {
+                        int flag = 0;
+                        for (int i = 0; i < countSeq; i++) {
+                            if (sequence[countSeq] == sequence[i]) {
+                                flag = 1;
+                                correct++;
+                                break;
+                            }
                         }
-                    }
-                    if(flag == 0){
-                        wrong++;
+                        if (flag == 0) {
+                            wrong++;
+                        }
                     }
                 }
             }
@@ -236,6 +255,19 @@ public class twoBack extends AppCompatActivity {
 
 
 
+    }
+
+    public void randomizeSeq(String letters) {
+        //Create new randomize object
+        Random rand = new Random();
+
+        for (int i = 0; i < sequence.length; i++) {
+            if (i % 2 == 1) {
+                sequence[i] = '.';
+            } else {
+                sequence[i] = letters.charAt(rand.nextInt(5));
+            }
+        }
     }
 }
 
