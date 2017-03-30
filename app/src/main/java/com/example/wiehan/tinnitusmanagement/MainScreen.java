ac0588754207a5a667e7d224d7eb9cce66719e2d
Wiehan <boshoff.2@wright.edu>
2017-03-30 14:48:45 -0400

    Added extra logos, swicth exit and start button on ABC game

--- app/src/main/java/com/example/wiehan/tinnitusmanagement/MainScreen.java
+++ app/src/main/java/com/example/wiehan/tinnitusmanagement/MainScreen.java
@@ -1,5 +1,6 @@
 package com.example.wiehan.tinnitusmanagement;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -17,12 +19,28 @@ public class MainScreen extends AppCompatActivity {
         setContentView(R.layout.activity_main_screen);
 
         start = (Button)findViewById(R.id.buttonMain);
+        Button about = (Button)findViewById(R.id.buttonAbout) ;
+
+        final Context context = getApplicationContext();
+        final CharSequence text = "Created by Interactions Design and Modeling Lab. Copyright © 2017" ;
+        final int duration = Toast.LENGTH_SHORT ;
+        final Toast toast = Toast.makeText(context, text, duration) ;
+
+        about.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                toast.makeText(context, text, duration).show();
+            }
+        });
+
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startButtonAnimation(start);
             }
         });
+
+
     }
 
     public void startButtonAnimation(Button startButton) {
