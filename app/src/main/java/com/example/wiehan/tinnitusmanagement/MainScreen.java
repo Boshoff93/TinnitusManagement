dbece61c384b5288c6034a74779855a0285363f6
Wiehan Boshoff <wiehan.boshoff@gmail.com>
2017-09-08 17:48:12 -0400

    Add functionalty to switch device to Silent Mode and prompt the user if Airplane mode is not enabled

--- app/src/main/java/com/example/wiehan/tinnitusmanagement/MainScreen.java
+++ app/src/main/java/com/example/wiehan/tinnitusmanagement/MainScreen.java
@@ -1,14 +1,18 @@
 package com.example.wiehan.tinnitusmanagement;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -28,7 +32,6 @@ public class MainScreen extends AppCompatActivity {
         final CharSequence text = "Created by Interactions Design and Modeling Lab. Copyright © 2017";
         final int duration = Toast.LENGTH_SHORT;
         final Toast toast = Toast.makeText(context, text, duration);
-
         final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 
         about.setOnClickListener(new View.OnClickListener() {
@@ -47,12 +50,71 @@ public class MainScreen extends AppCompatActivity {
                         vibe.vibrate(100);
                         break;
                     case MotionEvent.ACTION_UP:
-                        Intent intent = new Intent(getApplicationContext(), BreathTutorialOne.class);
-                        startActivity(intent);
-                        break;
+                        final AlertDialog.Builder silentAppAlert = new AlertDialog.Builder(MainScreen.this);
+                        silentAppAlert.setPositiveButton(
+                                "Yes",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                        Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                                        startActivity(intent);
+                                    }
+                                });
+
+                        silentAppAlert.setNegativeButton(
+                                "No",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                });
+
+                        final AlertDialog.Builder airPlaneModeAlert = new AlertDialog.Builder(MainScreen.this);
+                        airPlaneModeAlert.setPositiveButton(
+                                "OK",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int id) {
+                                    }
+                                });
+
+                        //Make sure users phone is in silent mode before proceeding
+                        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+                        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N && !notificationManager.isNotificationPolicyAccessGranted()) {
+                            final AlertDialog silentAlert = silentAppAlert.create();
+                            silentAlert.getWindow().setBackgroundDrawableResource(R.drawable.textview_custom);
+                            silentAlert.setMessage("IN ORDER FOR THE EXERCISE TO BE SUCCESSFUL, DO NOT DISTURB PERMISSON IS REQUIRED TO SWITCH THE DEVICE TO SILENT MODE. DO YOU WISH TO CONTINUE");
+                            silentAlert.show();
+                        } else {
+                            boolean isEnabled = Settings.System.getInt(
+                                    getContentResolver(),
+                                    Settings.System.AIRPLANE_MODE_ON, 0) == 1;
+                            if(!isEnabled) {
+                                final AlertDialog airPlaneMode = airPlaneModeAlert.create();
+                                airPlaneMode.getWindow().setBackgroundDrawableResource(R.drawable.textview_custom);
+                                airPlaneMode.setMessage("TO CONTINUE PLEASE SWITCH THE DEVICE TO AIRPLANE MODE TO PREVENT ANY DISTRACTIONS");
+                                airPlaneMode.show();
+                            } else {
+                                AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                                if (audioManager.getRingerMode() != AudioManager.RINGER_MODE_SILENT) {
+                                    audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                                    Intent intent = new Intent(getApplicationContext(), BreathTutorialOne.class);
+                                    startActivity(intent);
+                                } else {
+                                    Intent intent = new Intent(getApplicationContext(), BreathTutorialOne.class);
+                                    startActivity(intent);
+                                }
+                            }
+                        }
+
                 }
                 return false;
             }
         });
+
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+
+
     }
 }
